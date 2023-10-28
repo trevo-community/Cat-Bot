ca3e5c79a7155f9511d9f9d91d7bce3dac22d195
trevo-community <147546698+trevo-community@users.noreply.github.com>
2023-10-28 20:25:00 -0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-const anikit = require('trevo-api');
-const username = 'Clover';
-const key = 'Clover';
-
-anikit.playMP4("edite anuime funk minha xota e hit", username, key).then((response) => {
-  //console.log(response);
-  });
